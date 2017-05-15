bf3d2de5005904032d3666d9f13989934e99b035
jiro <gitromaru666>
2017-05-15 20:00:03 +0900

    変数検索スクリプトの追加と拡張

--- src/app/MainController.java
+++ src/app/MainController.java
@@ -19,98 +19,130 @@ import javafx.stage.Stage;
 
 public class MainController {
   @FXML private TextField searchVarTextField;
-  @FXML private TableView<MVVars> tableView;
-  @FXML private TableColumn<MVVars, Integer> idColumn;
-  @FXML private TableColumn<MVVars, String> nameColumn;
+
+  @FXML private TableView<MVVars> varTableView;
+  @FXML private TableColumn<MVVars, Integer> varIdColumn;
+  @FXML private TableColumn<MVVars, String> varNameColumn;
+
+  @FXML private TableView<MVVars> switchTableView;
+  @FXML private TableColumn<MVVars, Integer> switchIdColumn;
+  @FXML private TableColumn<MVVars, String> switchNameColumn;
+
   @FXML private ToggleButton toggleButton;
 
   private final DirectoryChooser dc = new DirectoryChooser();
   private File openedDir = null;
-  private ObservableList<MVVars> masterData;
+  private ObservableList<MVVars> varMasterData;
+  private ObservableList<MVVars> switchMasterData;
   private static final String SEP = File.separator;
 
   @FXML private void initialize() {//{{{
     dc.setInitialDirectory(new File("."));
 
-    idColumn  . setCellValueFactory(new PropertyValueFactory<MVVars, Integer>("id"));
-    nameColumn. setCellValueFactory(new PropertyValueFactory<MVVars, String>("name"));
+    varIdColumn  . setCellValueFactory(new PropertyValueFactory<MVVars, Integer>("id"));
+    varNameColumn. setCellValueFactory(new PropertyValueFactory<MVVars, String>("name"));
 
-    tableView.setOnMouseClicked(e -> {
-      if (e.getClickCount() == 2) {
-        MVVars item = tableView.getSelectionModel().getSelectedItem();
-        int id = item.idProperty().get();
-        invoke(id);
-      }
+    switchIdColumn  . setCellValueFactory(new PropertyValueFactory<MVVars, Integer>("id"));
+    switchNameColumn. setCellValueFactory(new PropertyValueFactory<MVVars, String>("name"));
+
+    final String SEARCH_VARS_SCRIPT     = "search_vars.vbs";
+    final String SEARCH_SWITCHES_SCRIPT = "search_switches.vbs";
+
+    // ダブルクリックで実行
+    varTableView.setOnMouseClicked(e -> {
+      doubleClick(varTableView, SEARCH_VARS_SCRIPT, e);
     });
-    masterData = FXCollections.observableArrayList();
-    //masterData.add(new MVVars(0, ""));
+    varMasterData = FXCollections.observableArrayList();
+
+    // ダブルクリックで実行
+    switchTableView.setOnMouseClicked(e -> {
+      doubleClick(switchTableView, SEARCH_SWITCHES_SCRIPT, e);
+    });
+    switchMasterData = FXCollections.observableArrayList();
 
     // テーブルのフィルタリング
-    FilteredList<MVVars> filteredData = new FilteredList<>(masterData, p -> true);
+    FilteredList<MVVars> varFilter = new FilteredList<>(varMasterData, p -> true);
+    FilteredList<MVVars> switchFilter = new FilteredList<>(switchMasterData, p -> true);
     searchVarTextField.textProperty().addListener((obs, oldVal, newVal) -> {
-      filteredData.setPredicate(db -> existsMatchedText(db, newVal));
+      varFilter.setPredicate(db -> existsMatchedText(db, newVal));
+      switchFilter.setPredicate(db -> existsMatchedText(db, newVal));
     });
-    tableView.setItems(filteredData);
+    varTableView.setItems(varFilter);
+    switchTableView.setItems(switchFilter);
 
     // キーマッピング
-    tableView.setOnKeyPressed(e -> {
-      if (!tableView.getSelectionModel().isEmpty()) {
-        final int PAGE_SIZE = 20;
-        if (KeyCode.J == e.getCode() && e.isControlDown()) {
-          for (int i=0; i<PAGE_SIZE; i++) {
-            selectNext();
-          }
-        } else if (KeyCode.K == e.getCode() && e.isControlDown()) {
-          for (int i=0; i<PAGE_SIZE; i++) {
-            selectPrevious();
-          }
-        } else if (KeyCode.J == e.getCode()) {
-          selectNext();
-        } else if (KeyCode.K == e.getCode()) {
-          selectPrevious();
-        } else if (KeyCode.ENTER == e.getCode()) {
-          MVVars item = tableView.getSelectionModel().getSelectedItem();
-          int id = item.idProperty().get();
-          invoke(id);
-        }
-      }
+    varTableView.setOnKeyPressed(e -> {
+      onKeyPressed(varTableView, SEARCH_VARS_SCRIPT, e);
     });
 
-    // MV側の選択を操作
-    //tableView.getFocusModel().focusedCellProperty().addListener((obs, oldVal, newVal) -> { });
+    // キーマッピング
+    switchTableView.setOnKeyPressed(e -> {
+      onKeyPressed(switchTableView, SEARCH_SWITCHES_SCRIPT, e);
+    });
 
   }//}}}
 
+  private void doubleClick(TableView<MVVars> table, String scriptFile, MouseEvent e) {//{{{
+    if (e.getClickCount() == 2) {
+      MVVars item = table.getSelectionModel().getSelectedItem();
+      int id = item.idProperty().get();
+      invoke(id, scriptFile);
+    }
+  }//}}}
+
+  private void onKeyPressed(TableView<MVVars> table, String scriptFile, KeyEvent e) {//{{{
+    if (!table.getSelectionModel().isEmpty()) {
+      final int PAGE_SIZE = 20;
+      if (KeyCode.J == e.getCode() && e.isControlDown()) {
+        for (int i=0; i<PAGE_SIZE; i++) {
+          selectNext(table);
+        }
+      } else if (KeyCode.K == e.getCode() && e.isControlDown()) {
+        for (int i=0; i<PAGE_SIZE; i++) {
+          selectPrevious(table);
+        }
+      } else if (KeyCode.J == e.getCode()) {
+        selectNext(table);
+      } else if (KeyCode.K == e.getCode()) {
+        selectPrevious(table);
+      } else if (KeyCode.ENTER == e.getCode()) {
+        MVVars item = table.getSelectionModel().getSelectedItem();
+        int id = item.idProperty().get();
+        invoke(id, scriptFile);
+      }
+    }
+  }//}}}
+
   @FXML private void openProject() {//{{{
-    File file = dc.showDialog(tableView.getScene().getWindow());
+    File file = dc.showDialog(varTableView.getScene().getWindow());
     if (file != null) {
       openedDir = file;
       dc.setInitialDirectory(file.getParentFile());
 
-      masterData.clear();
+      varMasterData.clear();
       readVarsList(new File(file.toString() + SEP + "data" + SEP + "System.json"));
-      Stage stage = (Stage) tableView.getScene().getWindow();
+      Stage stage = (Stage) varTableView.getScene().getWindow();
       stage.setTitle(file.getPath() + " - " + Main.TITLE_VERSION);
 
-      tableView.getSelectionModel().selectFirst();
+      varTableView.getSelectionModel().selectFirst();
     }
   }//}}}
 
   @FXML private void reload() {//{{{
     if (openedDir != null) {
-      masterData.clear();
+      varMasterData.clear();
       readVarsList(new File(openedDir.toString() + SEP + "data" + SEP + "System.json"));
-      tableView.getSelectionModel().selectFirst();
+      varTableView.getSelectionModel().selectFirst();
     }
   }//}}}
 
   @FXML private void switchAlwaysOnTop() {//{{{
     boolean isSelected = toggleButton.isSelected();
-    Stage stage = (Stage) tableView.getScene().getWindow();
+    Stage stage = (Stage) varTableView.getScene().getWindow();
     stage.setAlwaysOnTop(isSelected);
   }//}}}
 
-  private boolean existsMatchedText(MVVars db, String newVal) {//{{{
+  private static final boolean existsMatchedText(MVVars db, String newVal) {//{{{
     if (newVal == null || newVal.isEmpty()) {
       return true;
     }
@@ -142,6 +174,7 @@ public class MainController {
   }//}}}
 
   private void readVarsList(File file) {//{{{
+
     try {
       ObjectMapper mapper = new ObjectMapper();
       JsonNode root       = mapper.readTree(file);
@@ -150,31 +183,46 @@ public class MainController {
 
       IntStream.range(1, size).forEach(i -> {
         String name = child.get(i).asText();
-        masterData.add(new MVVars(i, name));
+        varMasterData.add(new MVVars(i, name));
       });
     } catch (IOException e) {
       e.printStackTrace();
     }
-  }//}}}
 
-  public void selectPrevious() {//{{{
-    int index = tableView.getSelectionModel().getSelectedIndex();
-    index = Math.max(0, --index);
-    tableView.getSelectionModel().clearAndSelect(index);
-    tableView.scrollTo(index);
-  }//}}}
-
-  public void selectNext() {//{{{
-    int index = tableView.getSelectionModel().getSelectedIndex();
-    int max = tableView.getItems().size();
-    index = Math.min(++index, max);
-    tableView.getSelectionModel().clearAndSelect(index);
-    tableView.scrollTo(index);
-  }//}}}
-
-  public void invoke(int id) {//{{{
     try {
-      Runtime.getRuntime().exec("cmd /c search_vars.vbs " + id);
+      ObjectMapper mapper = new ObjectMapper();
+      JsonNode root       = mapper.readTree(file);
+      JsonNode child      = root.get("switches");
+      int size = child.size();
+
+      IntStream.range(1, size).forEach(i -> {
+        String name = child.get(i).asText();
+        switchMasterData.add(new MVVars(i, name));
+      });
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+  }//}}}
+
+  public void selectPrevious(TableView<MVVars> table) {//{{{
+    int index = table.getSelectionModel().getSelectedIndex();
+    index = Math.max(0, --index);
+    table.getSelectionModel().clearAndSelect(index);
+    table.scrollTo(index);
+  }//}}}
+
+  public void selectNext(TableView<MVVars> table) {//{{{
+    int index = table.getSelectionModel().getSelectedIndex();
+    int max = table.getItems().size();
+    index = Math.min(++index, max);
+    table.getSelectionModel().clearAndSelect(index);
+    table.scrollTo(index);
+  }//}}}
+
+  public void invoke(int id, String scriptFileName) {//{{{
+    try {
+      Runtime.getRuntime().exec("cmd /c " + scriptFileName + " " + id);
     } catch (IOException e) {
       e.printStackTrace();
     }
